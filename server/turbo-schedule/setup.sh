0afd6f30a05d3cdd54cdeb0ebf2b39a20553588a
Kipras Melnikovas <kipras@kipras.org>
2024-03-01 19:19:56 +0200

    create server/turbo-schedule

---
+++ server/turbo-schedule/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+DIRNAME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd "$DIRNAME"
+. "../../util.sh"
+. "./vars.sh"
+
+REPO="turbo-schedule"
+BRANCH="whole-week-schedule" # TODO FIXME
+clone_forked_repo
+
+(
+	cd "$REPO_ROOT"
+	./run-docker.sh
+)
+
+install_nginx_site_with_replace "tvarkarastis.com" "PORT" "CONFIG_URL"
+
+>&2 printf "\ndeployed turbo-schedule. though, for full deployment, re-deploy yourself thru turbo-schedule/deploy.sh\n\n"
+
