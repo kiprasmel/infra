3d95e170f6ef1047619a6c770bb5645f079349bf
Kipras Melnikovas <kipras@kipras.org>
2024-03-15 18:18:46 +0200

    WIP: create local/devel-core-git

---
+++ local/devel-core-git/install-dotfiles.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# should be standalone, because will be used
+# while building the docker image
+
+set -euo pipefail
+set -x
+
+# TODO extract
+# TODO ssh?
+DOTFILES_REPO="https://github.com/kiprasmel/voidrice"
+
+(
+	cd "$HOME"
+	REPO_ROOT="$HOME/voidrice"
+	git clone --depth=1 "$DOTFILES_REPO" "$REPO_ROOT"
+
+	(
+		# see infra/server/dotfiles/setup.sh
+		cd "$REPO_ROOT"
+		mv "$REPO_ROOT/.git" "$HOME/.git"
+		rm -rf "$REPO_ROOT"
+		cd "$HOME"
+		git reset --hard HEAD # we don't care about overwriting something because new user
+		mv "$HOME/.git" "$HOME/.dotfiles"
+		# manage via `config` instead of via `git`
+	)
+
+	# gpg key warning
+	mkdir -p "$HOME/.config"
+	touch "$HOME/.config/.shell-warn-off"
+
+	# ~/.config/aliasrc
+	config() {
+		git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" -c commit.gpgSign=false "$@"
+	}
+
+	# patch
+	echo "" > ~/.config/nvim/init.vim
+	echo "" > ~/.gdbinit
+	config add ~
+	config commit -m "patch docker container"
+
+	which zsh || sudo apt install -y zsh
+	chsh -s "$(which zsh)" "$USER"
+)
+
